e921a1f7ad0a51d37d8b6cf56c79d0760f5ed7fe
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-09-14 11:56:17 -0600

    Add BuildListItem and loadItems functions, toggle name class on click

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,3 @@
-// SETUP
-
 // FUNCTIONS
 const toggleDoor = () => {
   $('.garage-door').toggleClass('garage-open');
@@ -12,8 +10,8 @@ const addItem = (e) => {
 }
 
 const postItem = () => {
-  const name = $('.item-name').val();
-  const reason = $('.item-reason').val();
+  const name = $('.item-name-input').val();
+  const reason = $('.item-reason-input').val();
   const cleanliness = $('.cleanliness-dropdown').val();
   
   fetch('/api/v1/item', {
@@ -28,17 +26,65 @@ const postItem = () => {
     }),
   })
     .then(res => res.json())
-    .then(item => console.log(item))
+    .then(item => buildListItem([item]))
     .catch(error => console.log({ error }));
 }
 
 const clearInputs = () => {
-  $('.item-name').val('');
-  $('.item-reason').val('');
+  $('.item-name-input').val('');
+  $('.item-reason-input').val('');
   $('.cleanliness-dropdown').val('Select a cleanliness...');
-  $('.item-name').focus();
+  $('.item-name-input').focus();
 }
+
+const buildListItem = (array) => {
+  if (!array.length) $('.items-display').text('Your garage is empty!  I don\'t believe it.');
+  
+  array.forEach((item) => {
+      $('.items-display').prepend(`
+        <div class="item-card">
+          <div class="item-name collapsed">
+            ${item.name}
+            <div class="item-details">
+              <p class="item-reason">${item.reason}</p>
+              <label for="cleanliness-dropdown">
+                Cleanliness:
+                <select
+                  class="cleanliness-dropdown"
+                  name="cleanliness-dropdown"
+                  required
+                >
+                  <option disabled>Change cleanliness...</option>
+                  <option value="Sparkling">Sparkling</option>
+                  <option value="Dusty">Dusty</option>
+                  <option value="Rancid">Rancid</option>
+                </select>
+              </label>
+            </div>
+          </div>
+        </div>
+      `)
+  })
+}
+
+const loadItems = () => {
+  fetch('api/v1/item')
+    .then(res => res.json())
+    .then(items => buildListItem(items))
+    .catch(error => console.log({ error }))
+}
+
+const toggleDetails = (e) => {
+  $(e.target).toggleClass('collapsed');
+  
+  // $('.garage-door').toggleClass('garage-open');
+
+}
+
+// SETUP
+loadItems();
 
 // EVENT LISTENERS
 $('.garage-door').on('click', toggleDoor);
 $('.new-item-form').on('submit', addItem);
+$('.items-display').on('click', '.item-name', toggleDetails);
